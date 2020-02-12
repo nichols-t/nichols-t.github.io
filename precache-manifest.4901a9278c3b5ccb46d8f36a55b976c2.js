cbf5c6a6c422d291577830b2f871eca0f347ee2e
Ty Nichols <nichols.t@husky.neu.edu>
2020-02-11 22:29:01 -0500

    Updates

---
+++ precache-manifest.4901a9278c3b5ccb46d8f36a55b976c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d94e8f4900981c066a58a52a26248874",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a62dc4747f1f244dc8b",
+    "url": "/static/js/2.cc55dbbc.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.cc55dbbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b826fa4640b43b7b220e",
+    "url": "/static/js/main.69c4f13b.chunk.js"
+  },
+  {
+    "revision": "4245780f655daa7034b0",
+    "url": "/static/js/runtime-main.88831461.js"
+  }
+]);
