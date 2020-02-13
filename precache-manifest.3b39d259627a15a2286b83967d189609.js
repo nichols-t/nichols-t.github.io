b23afa6962c117366a96a7428861747283736b58
Ty Nichols <nichols.t@husky.neu.edu>
2020-02-13 00:10:58 -0500

    Updates

---
+++ precache-manifest.3b39d259627a15a2286b83967d189609.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4dd04ad7302d98069e6fe786d0ffd23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a4e362523c24e008daa",
+    "url": "/static/js/2.057e4218.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.057e4218.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b3b44a1f83a08cf5805",
+    "url": "/static/js/main.1c03cc79.chunk.js"
+  },
+  {
+    "revision": "4245780f655daa7034b0",
+    "url": "/static/js/runtime-main.88831461.js"
+  },
+  {
+    "revision": "ccc722c15d88d5b399af96b4f24f1f53",
+    "url": "/static/media/newport-sunset-blur.ccc722c1.png"
+  }
+]);
--- src/header/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Header, Anchor, Button } from 'grommet';
-import { Home } from 'grommet-icons';
-
-const HeaderBar = () => <Header height="xxsmall" justify="end">
-    <Anchor as={Button} fill="vertical" margin="small" hoverIndicator icon={<Home color="light-2"/>} href="index.html"/>
-</Header>;
-
-export default HeaderBar;
