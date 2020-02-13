890a59dd05d2d3bcb78d4bb713eb0f9dcff1e476
Ty Nichols <nichols.t@husky.neu.edu>
2020-02-13 00:15:16 -0500

    Centered icons in nav.

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Header, Anchor, Button } from 'grommet';
 import { Home } from 'grommet-icons';
 
-const HeaderBar = () => <Header height="xxsmall" justify="end">
+const HeaderBar = () => <Header height="xxsmall" justify="end" align="center">
     <Anchor as={Button} fill="vertical" margin="small" hoverIndicator icon={<Home color="light-2"/>} href="index.html"/>
 </Header>;
 
