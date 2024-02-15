ebc5fef58ad25f41696dd73ceba9d6d85cec9e6a
nichols-t <tynichols0@gmail.com>
2024-02-14 21:03:03 -0500

    Updates

---
+++ precache-manifest.e5b9b06731f0862693c5a05948992a71.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56bef64efc769401d9dd9127015a985d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9136734aad0de1feb9cb",
+    "url": "/static/js/2.04fb2745.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.04fb2745.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2652d1a8130746e2c251",
+    "url": "/static/js/main.e0222f3e.chunk.js"
+  },
+  {
+    "revision": "b074553771ee999a7bee",
+    "url": "/static/js/runtime-main.f8fb009a.js"
+  },
+  {
+    "revision": "c8e618245ad82121592848cab8a59d9a",
+    "url": "/static/media/Eternal_Return.c8e61824.pdf"
+  },
+  {
+    "revision": "6ee74539685c8e7c4af16342ff03c59c",
+    "url": "/static/media/Final_Catch.6ee74539.pdf"
+  },
+  {
+    "revision": "8e7f6ac28cc6e2d0934fb5753b1c151b",
+    "url": "/static/media/Geralt_of_Rivia.8e7f6ac2.pdf"
+  },
+  {
+    "revision": "a0b0ab20d41d91a84e9bc1edbdd78d8f",
+    "url": "/static/media/Memories_of_Mother.a0b0ab20.pdf"
+  },
+  {
+    "revision": "710de573c30ae36a6a66617346167edf",
+    "url": "/static/media/Requiem_for_the_Brigadier_General.710de573.pdf"
+  },
+  {
+    "revision": "0af3b9802557fb1c048f868a2b9b12b5",
+    "url": "/static/media/Thin_Line.0af3b980.pdf"
+  }
+]);
